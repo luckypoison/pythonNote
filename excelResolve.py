863c23b99195f10120ace21f1340f6e1617a7214
harry <912043310@qq.com>
2019-12-21 22:16:07 +0800

    添加了一个修改excel的方法

--- excelResolve.py
+++ excelResolve.py
@@ -1,6 +1,6 @@
 # 引入opnepyxl来解析excel
 import openpyxl as xl
-
+import os
 
 # 通过excel名字获取excel的workbook
 def get_excel(excel_name):
@@ -20,13 +20,24 @@ def get_cells(excel_sheet, cell_positions):
     return cells
 
 
+# 在对应的cell中写入内容
+def write_cell(excel_sheet, cell_position, content):
+    excel_sheet[cell_position].value = content
+
+
+# 更改文件之后必须调用
+def update_excel(workbook, excel_name):
+    os.remove(excel_name)
+    workbook.save(excel_name)
+
 # 测试的main函数
 def test_code():
-    workbook = get_excel('test.xlsx')
+    excel_name = 'test.xlsx'
+    workbook = get_excel(excel_name)
     sheet = get_sheet(workbook, 'Sheet2')
     cells = get_cells(sheet, ['a1', 'a5'])
-    print(cells[0].value)
-    print(cells[1].value)
+    write_cell(sheet, 'a11', 'aaaaaaa')
+    update_excel(workbook, excel_name)
 
 
 test_code()
